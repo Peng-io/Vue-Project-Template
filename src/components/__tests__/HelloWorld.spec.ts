20fcca6436f96a9f3be8d7a1be7131133b72d084
pengsh <pee1944282957@163.com>
2025-03-14 15:27:29 +0800

    init

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,8 +1,5 @@
 import { describe } from "vitest"
-import {getStudentFind} from "@/api/methods/student";
 
-
-describe("HelloWorld", async () => {
-	let newVar = await getStudentFind(1,10);
-	console.log(newVar)
+describe("HelloWorld", () => {
+	console.log("HelloWorld")
 })
